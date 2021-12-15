5e50996bb9e7c28d03f182b358a6412ce07e5561
chaenabi <find1086@gmail.com>
2021-12-16 01:10:26 +0900

    Add README.md: 요구사항 명세 작성

--- src/main/java/study/tdd/simpleboard/SimpleBoardApplication.java
+++ src/main/java/study/tdd/simpleboard/SimpleBoardApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimpleBoardApplication.class, args);
     }
-
 }
