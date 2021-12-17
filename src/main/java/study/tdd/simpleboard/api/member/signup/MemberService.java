599321e769742dde0fce17bf4955f04a14916c22
pursue503 <pursue503@naver.com>
2021-12-17 23:47:03 +0900

    Update MemberService: validateNickname 3글자 검증 로직 구현 : 페어프로그래밍

--- src/main/java/study/tdd/simpleboard/api/member/signup/MemberService.java
+++ src/main/java/study/tdd/simpleboard/api/member/signup/MemberService.java
@@ -4,4 +4,11 @@ public class MemberService {
     public boolean validateSignUpParam(String nickname, String password) {
         return nickname != null && password != null;
     }
+
+    public boolean validateNickname(String nickname) {
+        if(nickname.length() >= 3) {
+            return true;
+        }
+        return false;
+    }
 }
