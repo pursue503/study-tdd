2bf590377d0ac6b6be261c24645c0bd5bdf4164e
pursue503 <pursue503@naver.com>
2021-12-18 21:19:50 +0900

    프로젝트 TDD && 페어 프로그래밍 실습 (#2)
    * Add MemberEntityTest: Member 엔티티 테스트코드 작성: 페어프로그래밍
    
    * Add MemberEntity: Member 엔티티 구현: 페어프로그래밍
    
    * Add MemberRepositoryTest: MemberRepository 테스트코드 작성: 페어프로그래밍
    
    * Add MemberRepository: MemberRepository 구현: 페어프로그래밍
    
    * Add MemberServiceTest: validateSignUpParamTest: 페어프로그래밍
    
    * Add MemberService: MemberService 구현: 페어 프로그래밍
    
    * Update MemberServiceTest: validateNickname 3글자 이상 테스트 기능 추가 : 페어프로그래밍
    
    * Update MemberService: validateNickname 3글자 검증 로직 구현 : 페어프로그래밍
    
    * Update README.md: 닉네임 3글자 이상 검증 요구사항 완료
    
    * Update MemberServiceTest: 알파벳 및 숫자 검증 테스트 추가 : 페어 프로그래밍
    
    * Update MemberService: 알파벳 및 숫자 검증 로직 구현 : 페어프로그래밍
    
    * Fix MemberService: 닉네임 검증 로직 리팩터링 : 페어 프로그래밍
    
    Co-authored-by: chaenabi <find1086@gmail.com>

---
+++ src/main/java/study/tdd/simpleboard/api/member/signup/MemberService.java
@@ -0,0 +1,39 @@
+package study.tdd.simpleboard.api.member.signup;
+
+public class MemberService {
+
+    private final int VALID_NICK_NAME_LENGTH = 3;
+
+    public boolean validateSignUpParam(String nickname, String password) {
+        return nickname != null && password != null;
+    }
+
+    public boolean validateNickname(String nickname) {
+        if (!validEnglish(nickname)) {
+            return false;
+        }
+        if (!validNumber(nickname)) {
+            return false;
+        }
+        if (!validLength(nickname)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validLength(String nickname) {
+        return nickname.length() >= VALID_NICK_NAME_LENGTH;
+    }
+
+    private boolean validEnglish(String nickname) {
+        return nickname.chars()
+                .filter(c -> (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+                .count() != 0;
+    }
+
+    private boolean validNumber(String nickname) {
+        return nickname.chars()
+                .filter(c -> c >= 48 && c<= 57)
+                .count() != 0;
+    }
+}
