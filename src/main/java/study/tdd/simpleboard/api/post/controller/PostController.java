29189ea90382e727aa3bc83144ebe3e2288ba679
chaenabi <find1086@gmail.com>
2021-12-21 22:35:59 +0900

    Add PostController.savePost(): 게시물 저장 메서드 추가

--- src/main/java/study/tdd/simpleboard/api/post/controller/PostController.java
+++ src/main/java/study/tdd/simpleboard/api/post/controller/PostController.java
@@ -1,10 +1,14 @@
-package study.tdd.simpleboard.api.post;
+package study.tdd.simpleboard.api.post.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import study.tdd.simpleboard.api.common.ResponseDTO;
+import study.tdd.simpleboard.api.post.domain.PostSaveRequestDTO;
+import study.tdd.simpleboard.exception.post.InvalidPostParameterException;
 
 import javax.validation.Valid;
 
@@ -13,11 +17,10 @@ import javax.validation.Valid;
 public class PostController {
 
     @PostMapping("/posts")
-    public ResponseDTO<PostSaveRequestDTO> savePost(@Valid PostSaveRequestDTO dto, BindingResult result) {
-        if (result.hasErrors()) throw new InvalidParameterException(result.getFieldErrors());
+    public ResponseDTO<PostSaveRequestDTO> savePost(@Valid @RequestBody PostSaveRequestDTO dto, BindingResult result) {
+        if (result.hasErrors()) throw new InvalidPostParameterException(result);
         // TODO: postService.save(dto)
         return new ResponseDTO<>(null, "게시물이 잘 저장되었습니다.", HttpStatus.OK);
     }
-
 
 }
