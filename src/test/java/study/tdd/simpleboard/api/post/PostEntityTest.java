e71fb760cf64b98b6970796dc58366b87c4ba2bb
chaenabi <find1086@gmail.com>
2021-12-21 14:38:14 +0900

    Add PostEntityTest: 비어있는 게시물 엔티티 테스트목록 작성

--- src/test/java/study/tdd/simpleboard/api/post/PostEntityTest.java
+++ src/test/java/study/tdd/simpleboard/api/post/PostEntityTest.java
@@ -3,6 +3,7 @@ package study.tdd.simpleboard.api.post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,38 +17,20 @@ public class PostEntityTest {
     }
 
     @Nested
-    @DisplayName("게시물 제목 테스트")
-    class PostTitleTest {
+    @DisplayName("게시물 제목 속성 테스트")
+    class PostTitlePropertyTest {
 
         @Test
-        @DisplayName("post 엔티티에 게시판 제목 속성이 있을 경우")
+        @DisplayName("post 엔티티에 게시물 제목 속성이 있고 정상 동작할 경우")
         public void createPostTitleProperty() {
-
+            Post post = Post.builder().postTitle("게시물 제목1").build();
+            assertThat(post.getPostTitle()).isNotNull().isEqualTo("게시물 제목1");
         }
-
-        @Test
-        @DisplayName("게시물 제목이 비어 있는 경우 400 에러 반환")
-        public void whenPostTitleisEmpty() {
-
-        }
-
-        @Test
-        @DisplayName("게시물 제목이 30글자를 초과하는 경우 400 에러 반환")
-        public void whenPostTitleExceeds30Letters() {
-
-        }
-
-        @Test
-        @DisplayName("게시물 제목이 1글자 이상 30 글자 이하일 경우")
-        public void isPostTitleBetween1and30Letters() {
-
-        }
-
     }
 
     @Nested
-    @DisplayName("게시물 내용 테스트")
-    class PostContentTest {
+    @DisplayName("게시물 내용 속성 테스트")
+    class PostContentPropertyTest {
 
         @Test
         @DisplayName("post 엔티티에 게시물 내용 속성이 있을 경우")
@@ -55,23 +38,25 @@ public class PostEntityTest {
 
         }
 
+    }
+
+    @Nested
+    @DataJpaTest
+    @DisplayName("Member와 Post 연관관계 테스트")
+    class _1Member_NPostRelationTest {
+
         @Test
-        @DisplayName("게시물 내용이 비어 있는 경우 400 에러 반환")
-        public void whenPostContentisEmpty() {
+        @DisplayName("존재하지 않는 회원은 게시물을 작성할 수 없음")
+        public void cannotPostExistByUnExistedMember() {
 
         }
 
         @Test
-        @DisplayName("게시물 내용이 2000글자를 초과하는 경우 400 에러 반환")
-        public void whenPostContentExceeds2000Letters() {
+        @DisplayName("존재하는 회원은 게시물 작성 가능")
+        public void canPostByExistedMember() {
 
         }
 
-        @Test
-        @DisplayName("게시물 내용이 1글자 이상 2000 글자 이하일 경우")
-        public void isPostContentBetween1and2000Letters() {
-
-        }
     }
 
 }
