088241846957708343171bcc6f360770adb84c6b
chaenabi <find1086@gmail.com>
2021-12-24 21:52:40 +0900

    Add describe savePost: 게시물 저장 메서드 설명 추가'

--- src/main/java/study/tdd/simpleboard/api/post/controller/PostController.java
+++ src/main/java/study/tdd/simpleboard/api/post/controller/PostController.java
@@ -1,11 +1,13 @@
 package study.tdd.simpleboard.api.post.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import study.tdd.simpleboard.api.common.ResponseDTO;
 import study.tdd.simpleboard.api.post.domain.PostSaveRequestDTO;
+import study.tdd.simpleboard.api.post.service.PostService;
 import study.tdd.simpleboard.exception.post.InvalidPostParameterException;
 import study.tdd.simpleboard.exception.post.PostCrudErrorCode;
 
@@ -20,14 +22,23 @@ import javax.validation.Valid;
  * @since 0.0.1 dev
  */
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 public class PostController {
 
+    private final PostService postService;
+
+    /**
+     * 게시물 저장 요청을 받아 저장 처리후 반환값으로 저장된 게시물의 postId를 반환합니다.
+     *
+     * @param dto    게시물 제목, 게시물 내용, 이미지 주소 (선택사항)
+     * @param result 제목 및 내용에 대한 규칙 검증 결과
+     * @return 성공적으로 저장된 게시물의 고유 아이디
+     */
     @PostMapping("/posts")
-    public ResponseDTO<PostSaveRequestDTO> savePost(@Valid @RequestBody PostSaveRequestDTO dto, BindingResult result) {
+    public ResponseDTO<Long> savePost(@Valid @RequestBody PostSaveRequestDTO dto, BindingResult result) {
         if (result.hasErrors()) throw new InvalidPostParameterException(result, PostCrudErrorCode.POST_CRUD_FAIL);
-        // TODO: postService.save(dto)
-        return new ResponseDTO<>(null, "게시물이 잘 저장되었습니다.", HttpStatus.OK);
+        return new ResponseDTO<>(postService.savePost(dto), "게시물이 잘 저장되었습니다.", HttpStatus.OK);
     }
 
     @GetMapping("/posts/{postId}")
