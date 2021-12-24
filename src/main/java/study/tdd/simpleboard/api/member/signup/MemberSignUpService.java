828cfdf0402f994688f9938e61bd442279b097e2
pursue503 <pursue503@naver.com>
2021-12-24 17:42:07 +0900

    Update MemberSignUpService : 유효성 검증 실패시 에러를 throw 하는 코드 구현

--- src/main/java/study/tdd/simpleboard/api/member/signup/MemberSignUpService.java
+++ src/main/java/study/tdd/simpleboard/api/member/signup/MemberSignUpService.java
@@ -4,6 +4,8 @@ import study.tdd.simpleboard.api.member.entity.Member;
 import study.tdd.simpleboard.api.member.entity.MemberRepository;
 import study.tdd.simpleboard.api.member.signup.dto.MemberSignUpRequestDTO;
 import study.tdd.simpleboard.api.member.signup.valid.Valid;
+import study.tdd.simpleboard.exception.common.BizException;
+import study.tdd.simpleboard.exception.member.signup.MemberSignUpErrorCode;
 
 public class MemberSignUpService {
 
@@ -22,19 +24,17 @@ public class MemberSignUpService {
 
     public Member saveMember(MemberSignUpRequestDTO memberSignUpRequestDTO) {
         if (!memberSignUpRequestDTO.validateParam()) {
-            return null;
+            throw new BizException(MemberSignUpErrorCode.SIGN_UP_PARAM_NULL_OR_EMPTY);
         }
         if (!validationNickname.verify(memberSignUpRequestDTO.getNickname())) {
-            return null;
+            throw new BizException(MemberSignUpErrorCode.SIGN_UP_NICKNAME_NOT_VALID);
 
         }
         if (!validationPassword.verify(memberSignUpRequestDTO.getPassword())) {
-            return null;
-
+            throw new BizException(MemberSignUpErrorCode.SIGN_UP_PASSWORD_NOT_VALID);
         }
         if (checkDuplicatedNickname(memberSignUpRequestDTO.getNickname())) {
-            return null;
-
+            throw new BizException(MemberSignUpErrorCode.SIGN_UP_NICKNAME_DUPLICATED);
         }
         return memberSignUpRequestDTO.toEntity();
     }
