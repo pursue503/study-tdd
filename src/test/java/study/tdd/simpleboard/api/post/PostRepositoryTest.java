d2f529b509909478abb2f60bdc203aefd09a5c7f
chaenabi <find1086@gmail.com>
2021-12-24 21:56:33 +0900

    Add TODO: 게시물 수정 삭제 TODO 작성

--- src/test/java/study/tdd/simpleboard/api/post/PostRepositoryTest.java
+++ src/test/java/study/tdd/simpleboard/api/post/PostRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import study.tdd.simpleboard.api.member.entity.Member;
+import study.tdd.simpleboard.api.member.signup.MemberRepository;
 import study.tdd.simpleboard.api.post.entity.Post;
 import study.tdd.simpleboard.api.post.repository.PostRepository;
 import study.tdd.simpleboard.exception.common.BizException;
@@ -17,6 +18,8 @@ import study.tdd.simpleboard.exception.post.PostCrudErrorCode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 /**
  * h2는 임베디드 DB를 사용하므로, inmemory 설정을 사용할 수 있도록
@@ -35,7 +38,8 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
-    Member member = Mockito.mock(Member.class);
+    Member member = mock(Member.class);
+    MemberRepository memberRepository = mock(MemberRepository.class);
 
     @Test
     @DisplayName("게시물 텍스트 저장")
@@ -123,27 +127,40 @@ public class PostRepositoryTest {
     @DisplayName("게시물 1개 수정")
     void updateOnePost() {
         // 준비
-        Post wantToFind = Post.builder()
-                .postTitle("30번 제목")
-                .postContent("30번 내용")
+        Post wantToChange = Post.builder()
+                .postTitle("제목 변경")
+                .postContent("내용 변경")
                 .member(member)
                 .image("/image.png")
                 .build();
 
         // 실행
-        Post saved = postRepository.save(wantToFind);
-        Post founded = postRepository.findById(saved.getPostId())
-                .orElseThrow(() -> new BizException(PostCrudErrorCode.POST_NOT_FOUND));
+        /*
+            TODO: SELECT p.post_id
+                  FROM member m inner join post p
+                  ON m.member_id = p.member_id
+                  WHERE EXISTS (
+                     SELECT p2.post_id
+                     FROM POST p2
+                     p2.member_id = :member.getId());
+
+            쿼리를 질의하여 존재하지 않으면:
+                throw new BizException(PostCrudErrorCode.PATCH_NOT_PERMIT) 반환
+            존재하면
+                UPDATE post
+                SET post_title=:wantToChange.getPostTitle() ....
+                WHERE post_id = :위에서 가져온 p.post_id
+         */
 
         // 검증
-        assertThat(wantToFind)
-                .isEqualTo(saved)
-                .isEqualTo(founded);
     }
 
     @Test
     @DisplayName("게시물 1개 삭제")
     void deleteOnePost() {
+
+        // TODO: memberRepository가 준비되는대로 재구현 대기중
+
         // 준비
         Post wantToDelete = Post.builder()
                 .postTitle("32번 제목")
@@ -162,14 +179,12 @@ public class PostRepositoryTest {
         assertThat(canFindPost).isEqualTo(saved);
 
         // 삭제 실행
-        postRepository.delete(saved);
+
 
         // 삭제 후 검증
         assertThatThrownBy(() -> {
             postRepository.findById(saved.getPostId())
                     .orElseThrow(() -> new BizException(PostCrudErrorCode.POST_NOT_FOUND));
         });
-
     }
-
 }
