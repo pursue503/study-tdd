28f5c66486327d122e14dd1291d39bb4b5ed038c
chaenabi <find1086@gmail.com>
2021-12-24 15:42:04 +0900

    Add PostService: 게시물을 1개 조회하는 로직 구현

--- src/main/java/study/tdd/simpleboard/api/post/service/PostService.java
+++ src/main/java/study/tdd/simpleboard/api/post/service/PostService.java
@@ -7,6 +7,14 @@ import study.tdd.simpleboard.api.post.repository.PostRepository;
 import study.tdd.simpleboard.exception.common.BizException;
 import study.tdd.simpleboard.exception.post.PostCrudErrorCode;
 
+/**
+ * 게시물 요청을 실제로 처리하는 서비스 객체
+ *
+ * @author Informix
+ * @create 2021-12-24
+ * @since 2.6.1 spring boot
+ * @since 0.0.1 dev
+ */
 @Service
 @RequiredArgsConstructor
 public class PostService {
