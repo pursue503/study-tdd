9631227b60f78e7b13c730e3c5cbed5bd42bec01
chaenabi <find1086@gmail.com>
2021-12-24 15:41:50 +0900

    Add PostRepository: PostRepository DataJPA 인터페이스 추가

--- src/main/java/study/tdd/simpleboard/api/post/repository/PostRepository.java
+++ src/main/java/study/tdd/simpleboard/api/post/repository/PostRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.tdd.simpleboard.api.post.entity.Post;
 
+/**
+ * Post Entity를 조작하는 기능을 제공하는 data-jpa 인터페이스
+ *
+ * @author Informix
+ * @create 2021-12-24
+ * @since 2.6.1 spring boot
+ * @since 0.0.1 dev
+ */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
