3b61381160ce6b0c882d323c8e55d595250c5913
chaenabi <find1086@gmail.com>
2021-12-24 21:54:29 +0900

    Add savePost Method: 게시물 저장 처리 구현

--- src/main/java/study/tdd/simpleboard/api/post/service/PostService.java
+++ src/main/java/study/tdd/simpleboard/api/post/service/PostService.java
@@ -2,21 +2,23 @@ package study.tdd.simpleboard.api.post.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import study.tdd.simpleboard.api.post.domain.PostSaveRequestDTO;
 import study.tdd.simpleboard.api.post.entity.Post;
 import study.tdd.simpleboard.api.post.repository.PostRepository;
 import study.tdd.simpleboard.exception.common.BizException;
 import study.tdd.simpleboard.exception.post.PostCrudErrorCode;
 
 /**
- * 게시물 요청을 실제로 처리하는 서비스 객체
+ * 게시물 CRUD 요청 처리 서비스
  *
  * @author Informix
  * @create 2021-12-24
  * @since 2.6.1 spring boot
  * @since 0.0.1 dev
  */
-@Service
+
 @RequiredArgsConstructor
+@Service
 public class PostService {
 
     private final PostRepository postRepository;
@@ -24,5 +26,10 @@ public class PostService {
     public Post findOnePost(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(() -> new BizException(PostCrudErrorCode.POST_NOT_FOUND));
+    }
+
+    public Long savePost(PostSaveRequestDTO dto) {
+        return postRepository.save(dto.toEntity())
+                             .getPostId();
     }
 }
