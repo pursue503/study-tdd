cc6ca292d9bf173be98f5ffb14f21cabdaa16f58
pursue503 <pursue503@naver.com>
2021-12-25 22:48:34 +0900

    Update PostControllerTest : WebMvcTest 에 PostController 만 사용 하도록 설정값 추가

--- src/test/java/study/tdd/simpleboard/api/post/PostControllerTest.java
+++ src/test/java/study/tdd/simpleboard/api/post/PostControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(PostController.class)
 public class PostControllerTest {
 
     private MockMvc mockmvc;
