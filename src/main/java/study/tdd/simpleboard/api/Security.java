56ffec3c23a4bed831660a00cf4aafeef44ed350
chaenabi <find1086@gmail.com>
2021-12-30 18:41:59 +0900

    change setting: web.ignoring().h2-console/**

--- src/main/java/study/tdd/simpleboard/api/Security.java
+++ src/main/java/study/tdd/simpleboard/api/Security.java
@@ -2,6 +2,7 @@ package study.tdd.simpleboard.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -16,11 +17,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class Security extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().mvcMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/posts/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll();
+                .antMatchers("/posts/**").permitAll();
 
         http.csrf()
                 .disable();
